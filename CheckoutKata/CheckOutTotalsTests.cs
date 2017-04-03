f80865e51e0ab3e63400588dc2eb660757e58599
Stephen Smith <stephen@beanland-smith.co.uk>
2017-04-03 11:21:04 +0100

    Renamed UnitTest1 Class to more suitable name

--- CheckoutKata/CheckOutTotalsTests.cs
+++ CheckoutKata/CheckOutTotalsTests.cs
@@ -4,7 +4,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace CheckoutKata
 {
     [TestClass]
-    public class UnitTest1
+    public class CheckOutTotalsTests
     {
         [TestMethod]
         public void OnAGivesTotalOf50()
