cd39c993cc40f17bd44f35cd9020068e26f295b3
Stephen Smith <stephen@beanland-smith.co.uk>
2017-04-03 14:01:37 +0100

    Tests to ensure various multiples of "A" results in correct totals.

---
+++ CheckoutKata/RegisterMultipleUnits.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using CheckoutKata;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CheckoutKataTests
+{
+    [TestClass]
+    public class CheckoutMultipleUnits
+    {
+        [TestMethod]
+        public void TwoAGivesTotalOf100()
+        {
+            var checkout = new CheckoutKata.Checkout();
+
+            checkout.Register("A");
+            checkout.Register("A");
+            checkout.Register("A");
+
+            Assert.AreEqual(130M, checkout.Total);
+        }
+
+        [TestMethod]
+        public void ThreeAGivesTotalOf130()
+        {
+            var checkout = new CheckoutKata.Checkout();
+
+            checkout.Register("A");
+            checkout.Register("A");
+            checkout.Register("A");
+
+            Assert.AreEqual(130M, checkout.Total);
+        }
+
+        [TestMethod]
+        public void FourAGivesTotalOf180()
+        {
+            var checkout = new CheckoutKata.Checkout();
+
+            checkout.Register("A");
+            checkout.Register("A");
+            checkout.Register("A");
+            checkout.Register("A");
+
+            Assert.AreEqual(180M, checkout.Total);
+        }
+
+        [TestMethod]
+        public void SixAGivesTotalOf260()
+        {
+            var checkout = new CheckoutKata.Checkout();
+
+            checkout.Register("A");
+            checkout.Register("A");
+            checkout.Register("A");
+            checkout.Register("A");
+            checkout.Register("A");
+            checkout.Register("A");
+
+            Assert.AreEqual(260M, checkout.Total);
+        }
+
+    }
+}
