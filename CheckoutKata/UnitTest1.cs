69f118ae1a699327ebca546f9fba16964d00e04b
Stephen Smith <stephen@beanland-smith.co.uk>
2017-04-03 11:18:10 +0100

    Creation of solution

---
+++ CheckoutKata/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CheckoutKata
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void OnAGivesTotalOf50()
+        {
+        }
+    }
+}
